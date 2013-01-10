6cc5e9f8bc2eb3989a36ab07a76903a13505f7d8
Luca Bellenghi <lucabel@gmail.com>
2013-01-10 14:38:45 +0100

    Preparing release 1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.2.dev0'
+version = '1.2'
 
 tests_require = ['zope.testing']
 
