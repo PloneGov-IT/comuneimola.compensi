f38a465536e8173f5137a486c1a37ee4755e793d
Luca Bellenghi <lucabel@gmail.com>
2013-02-14 17:48:36 +0100

    Preparing release 1.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.3.dev0'
+version = '1.3'
 
 tests_require = ['zope.testing']
 
